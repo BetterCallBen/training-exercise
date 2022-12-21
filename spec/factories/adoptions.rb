ee545798b227d6f8e88b1f3210d1b988e09c4110
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-12-21 17:18:39 +0100

    fix: typo for starts_at in spec/factories/adoptions (#3)

--- spec/factories/adoptions.rb
+++ spec/factories/adoptions.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     association :user
     association :tree
     name { "adoptionName" }
-    start_at { Date.today }
+    starts_at { Date.today }
     ends_at { Date.today + 1.year }
   end
 end
